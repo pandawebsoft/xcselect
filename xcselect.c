fe08a3397ac67970872ca0bef855e8b364e5953f
PandaWebsoft <developer@pandawebsoft.com>
2018-08-16 22:38:27 +0900

    Improved matching algorithm

--- xcselect.c
+++ xcselect.c
@@ -3,26 +3,27 @@
 #include <dirent.h>
 #include <string.h>
 
-#define VERSION "1.0.0(10)"
+#define VERSION "1.0.0(12)"
 
-#define GRN	"\x1B[32m"
+#define GRN "\x1B[32m"
 #define RESET "\x1B[0m"
 
 void usage();
 void print_version();
-char* find_xcode_path(const char *path, const char *keyword);
+void lntrim(char *str);
+char* find_xcode_path(const char *version);
 void switch_xcode_version(char *version);
 
 int main(int argc, char* argv[])
 {
-    if(argc == 2)
+    if (argc == 2)
     {
 	    char* argument = argv[1];
         if (strcmp(argument, "--version") == 0)
         {
             print_version();
         }
-        else if(strcmp(argument, "--help") == 0)
+        else if (strcmp(argument, "--help") == 0)
         {
             usage();
         }
@@ -47,40 +48,71 @@ void print_version()
     printf("%s\n", VERSION);
 }
 
-char* find_xcode_path(const char *path, const char *keyword)
-{
-    struct dirent *entry;
-    DIR *dir = opendir(path);
-    if (dir == NULL)
-    {
-        return "";
-    }
-
-    char *xcode_path = "";
-    while ((entry = readdir(dir)) != NULL)
-    {
-		if (strcasestr(entry->d_name, "xcode") != NULL && strstr(entry->d_name, keyword) != NULL)
-		{
-			char *tmp_path = "/Contents/Developer";
-			xcode_path = (char *) malloc(1 + strlen(path) + strlen(tmp_path));
-			strcpy(xcode_path, path);
-			strcat(xcode_path, entry->d_name);
-			strcat(xcode_path, tmp_path);
-		    break;
-		}
-	}
-
-    closedir(dir);
-    return xcode_path;
-}
-
 void switch_xcode_version(char *version) 
 {
-    char *path = find_xcode_path("/Applications/", version);
+    char *path = find_xcode_path(version);
+    if (path != NULL)
+    {
+        char buffer[255];
+        snprintf(buffer, sizeof(buffer), "sudo xcode-select -s \"%s\"", path);
+        system(buffer);
 
-    char buffer[255];
-    snprintf(buffer, sizeof(buffer), "sudo xcode-select -s \"%s\"", path);
-    system(buffer);
+        system("xcodebuild -version");
+    } 
+    else 
+    {
+        printf("Error: the specified version(%s) of Xcode was not found.\n", version);
+    }
+}
 
-    system("xcodebuild -version");
+char* find_xcode_path(const char *version)
+{
+    system("rm -rf /tmp/xcselect && mkdir /tmp/xcselect");
+    system("xcode_plist_paths=$(ls -l /Applications/ | grep -i \"xcode\" | awk '{print $9}');for p in $xcode_plist_paths; do echo `defaults read \"/Applications/${p}/Contents/Info.plist\" CFBundleShortVersionString`\",/Applications/${p}\" >> \"/tmp/xcselect/detected_xcode_versions\"; done");
+
+    FILE * fp;
+    char * line = NULL;
+    size_t len = 0;
+    ssize_t read;
+
+    fp = fopen("/tmp/xcselect/detected_xcode_versions", "r");
+    if (fp == NULL)
+        return NULL;
+
+    while ((read = getline(&line, &len, fp)) != -1) 
+    {
+        char *xcode_version = strsep(&line,",");
+        char *xcode_path = strsep(&line,",");
+        lntrim(xcode_path);
+        if (xcode_version != NULL && xcode_path != NULL) 
+        {
+            printf("version: %s\n",xcode_version);
+            printf("path: %s\n",xcode_path);
+            if (strstr(xcode_version, version) != NULL)
+            {
+                char *tmp_path = "/Contents/Developer";
+                char *found_path = (char *) malloc(1 + strlen(xcode_path) + strlen(tmp_path));
+                strcpy(found_path, xcode_path);
+                strcat(found_path, tmp_path);
+                printf("found_path: %s\n",found_path);
+                return found_path;
+            }
+        }
+    }
+
+    fclose(fp);
+    if (line)
+        free(line);
+
+    return NULL;
+}
+
+void lntrim(char *str)
+{  
+    char *p;  
+    p = strchr(str, '\n');  
+    if (p != NULL)
+    {  
+        *p = '\0';  
+    }  
 }
